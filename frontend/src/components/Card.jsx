1432757d60e19ccb7ea8cdff2c27df4605ffaeae
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-07-04 11:13:37 +0530

    changed frontend

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,25 +4,36 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import './Card.css';
 
 export default function ImgMediaCard({title, description}) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      className='card'
+      sx={{ maxWidth: 345 }}>
       <CardMedia
+        className='img'
         component="img"
-        alt="green iguana"
         height="140"
         image="/static/images/cards/contemplative-reptile.jpg"
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardContent
+        className='text'
+      >
+        <Typography 
+          className='title'
+          gutterBottom variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography 
+          className='description'
+          variant="body2" color="text.secondary">
           {description}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions
+        className='buttons'
+      >
         <Button size="small">Donate</Button>
         <Button size="small">Learn More</Button>
       </CardActions>
