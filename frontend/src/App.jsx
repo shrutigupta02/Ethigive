fb2c759dd19cac141dd4cb85f585c443996a4f58
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-07-04 23:56:34 +0530

    Implemented project details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Projects from "./pages/Projects"
 import AboutUs from "./pages/AboutUs"
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Register from "./pages/Register";
+import Project from "./pages/Project";
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
           <Route exact path = "/projects" element={<Projects/>}/>
           <Route exact path = "/us" element={<AboutUs/>}/>
           <Route exact path = "/register" element={<Register/>}/>
+          <Route path="/projects/:id" element={<Project/>} />
         </Routes>
       </BrowserRouter>
     </>
