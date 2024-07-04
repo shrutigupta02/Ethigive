218f17dd82e09acae29a1b42b9f057f50ffe0e63
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-07-04 22:12:16 +0530

    frontend

--- frontend/src/components/CardSlider.jsx
+++ frontend/src/components/CardSlider.jsx
@@ -8,7 +8,7 @@ export default function ResponsiveGrid( {projects}) {
     <Box sx={{ flexGrow: 1 }} className="card-slider-container">
       <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 2, sm: 4, md: 6 }}>
         {projects.map((project)=>(
-          <ImgMediaCard title={project.charityname}  description={project.description} image={project.image}/>
+          <ImgMediaCard title={project.charityname}  description={project.description} image={project.image} location={project.location}/>
         ))}
       </Grid>
     </Box>
