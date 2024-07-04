ba8639e766dde20989ae157a42150559a7d26423
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-07-04 13:28:21 +0530

    pages frontend

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,5 +1,22 @@
-export default function Projects(){
-    return(
-        <div>hello</div>
-    )
+import React from 'react';
+import Navbar from '../components/Navbar';
+import BackgroundImage from '../components/BackgroundImage';
+import Footer from '../components/Footer';
+import CardSlider from '../components/CardSlider';
+import './Projects.css';
+
+export default function Projects() {
+    return (
+        <div className='container'>
+            <Navbar />
+            <div className="hero-section">
+                <BackgroundImage />
+                <div className="hero-text">
+                    <h2>PROJECTS</h2>
+                </div>
+            </div>
+            <CardSlider />
+            <Footer />
+        </div>
+    );
 }
